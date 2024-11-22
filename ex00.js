44a19907b1efc22d4c1ea8eb0fbe04eb2fb56287
S29Ducky <barasrasonael@hotmail.com>
2024-11-22 14:18:47 +0100

    update ex00

--- ex00.js
+++ ex00.js
@@ -1,14 +1,10 @@
-// ex00.js
-
 function sumArray(arr) {
-    let resulta = 0; 
-
-    for(let i = 0; i < arr.length; i++){
-        resulta = resulta + arr [i]
+    let result = 0 ;
+    for (let i = 0 ; i < arr.length; i++){
+        result = result + arr[i]
     }
-    return resulta;
+    return result
 }
 
-// Exemples de test :
 console.log(sumArray([1, 2, 3, 4])); // Résultat attendu : 10
 console.log(sumArray([10, -2, 5])); // Résultat attendu : 13
